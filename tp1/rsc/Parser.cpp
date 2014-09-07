692566a2b61b33446daca84717d5781063765574
sasavulsky@yahoo.com.ar <sasavulsky@yahoo.com.ar@2f48152e-bfd8-70ec-e1fd-0a46247cd28f>
2014-09-07 20:41:24 +0000

    Se completó el Parser. Ahora asume valores por defecto para todas las variables y recorre los n objetos que tenga el archivo .json. Falta implementarle la creación ahora de todos los objetos Box2D que representan estas variables de escenario y objetos. La variable imagen_fondo servirá para indicar el nombre de la imagen de fondo en SDL.

--- tp1/rsc/Parser.cpp
+++ tp1/rsc/Parser.cpp
@@ -19,18 +19,38 @@ int main(){
     }
 
     std::string escenario;
-    std::string imagen_fondo;
     Json::Value un_Escenario;
     un_Escenario = root["escenario"];
        /* Busca imagen_fondo en el archivo json, si no lo encuentra crea un null
       	por defecto.*/
-       imagen_fondo = un_Escenario.get("imagen_fondo", "imagen no encontrada").asString();
-       std::cout << imagen_fondo <<"\n";
-       int alto_px = un_Escenario.get("alto-px", 0).asInt();
-       int ancho_px = un_Escenario.get("ancho-px", 0).asInt();
-       int alto_un = un_Escenario.get("alto-un", 0).asInt();
-       int ancho_un = un_Escenario.get("ancho-un", 0).asInt();
 
+    std::string imagen_fondo = un_Escenario.get("imagen_fondo", "fondo1.png").asString();
+    std::cout << imagen_fondo <<"\n";
+       int alto_px = un_Escenario.get("alto_px", 768).asInt();
+       int ancho_px = un_Escenario.get("ancho_px", 1024).asInt();
+       int alto_un = un_Escenario.get("alto_un", 100).asInt();
+       int ancho_un = un_Escenario.get("ancho_un", 50).asInt();
+       int personaje_x = un_Escenario["personaje"].get("x", 100).asInt();
+       int personaje_y = un_Escenario["personaje"].get("y", 100).asInt();
+
+       Json::Value objetos;
+       objetos = un_Escenario["objetos"];
+
+       //En esta parte parseamos todos los objetos que haya, y creamos los objetos correspondientes
+       for (size_t i = 0; i < objetos.size(); i++)
+       {
+           std::string tipo = objetos[i].get("tipo", "rect").asString();
+           int x = objetos[i].get("x", 5).asInt();
+           int y = objetos[i].get("y", 90).asInt();
+           int ancho = objetos[i].get("ancho", 2).asInt();
+           int alto = objetos[i].get("alto", 1).asInt();
+           int lados = objetos[i].get("lados", 2).asInt();
+           std::string color = objetos[i].get("color", "#FF0000").asString();
+           int rot = objetos[i].get("rot", 0).asInt();
+           int masa = objetos[i].get("masa", 1).asInt();
+           bool estatico = objetos[i].get("estatico", false).asBool();
+           float escala = objetos[i].get("escala", 1).asFloat();
+       }
        return 0;
 
 }
