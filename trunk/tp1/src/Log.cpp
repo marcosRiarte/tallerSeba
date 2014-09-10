546a33839119950e55863a3458b357cf43e2499f
sasavulsky@yahoo.com.ar <sasavulsky@yahoo.com.ar@2f48152e-bfd8-70ec-e1fd-0a46247cd28f>
2014-09-10 14:28:56 +0000

    Se modificaron las clases Log y Parser y se creo el creador de objetos a partir del parser.

--- trunk/tp1/src/Log.cpp
+++ trunk/tp1/src/Log.cpp
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 void Log::Loguear(std::string mensajeError, std::string nombreArchivo) {
-	std::string salidaStr = "Log.txt";
+	std::string salidaStr = "tpTaller_Log.txt";
 	std::ofstream salida;
 	salida.open(salidaStr.c_str());
 	salida << mensajeError + nombreArchivo << "\n";
