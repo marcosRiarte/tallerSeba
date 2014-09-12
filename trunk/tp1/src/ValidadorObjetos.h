7dd8f41391dad973d087642d31ac96be349e9ae6
sasavulsky@yahoo.com.ar <sasavulsky@yahoo.com.ar@2f48152e-bfd8-70ec-e1fd-0a46247cd28f>
2014-09-12 16:29:35 +0000

    Se modificaron las clases ValidadorObjetos para las validaciones de los objetos después del parseo.

--- trunk/tp1/src/ValidadorObjetos.h
+++ trunk/tp1/src/ValidadorObjetos.h
@@ -3,13 +3,12 @@
 
 class ValidadorObjetos {
 public:
-	ValidarBasicos(int x, int y,std::string color);
-	ValidarPoligono(int lados, int escala, int rotacion,int masa,bool estatico);
-	ValidarRectangulo();
-	ValidarCirculo();
-	ValidarParalelogramo();
-	ValidarTrapecio();
-	virtual ~ValidadorObjetos();
+	static bool ValidarBasicos(int x, int y, std::string color, int rotacion, int masa, bool estatico);
+	static bool ValidarPoligono(int lados, int escala);
+	static bool ValidarRectangulo(int ancho, int alto);
+	static bool ValidarCirculo(int diametro);
+	static bool ValidarParalelogramoNoRectangulo(int base, int ladoaltura, int alfa);
+	static bool ValidarTrapecio(int base, int altura, int alfa, int beta);
 };
 
 #endif /* VALIDADOROBJETOS_H_ */
