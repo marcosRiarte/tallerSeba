2febf03d89acae99dc991bc12217be5032d34153
Franlopd@gmail.com <Franlopd@gmail.com@2f48152e-bfd8-70ec-e1fd-0a46247cd28f>
2014-09-13 21:38:26 +0000

    Acomode los .h para escenario y termine evento

--- tp1/src/entrada/Evento.h
+++ tp1/src/entrada/Evento.h
@@ -5,21 +5,24 @@
 #ifndef EVENTO_H_
 #define EVENTO_H_
 
-#include <iostream>
-
-using namespace std;
 
 class Evento {
 private:
-	// No valía la pena hacer mas clases, acá ponemos los valores posibles:
-	// arriba, derecha, izquierda, nada.
-	string teclaIngresada;
+	int teclaIngresada;
 public:
+	// Valores posibles para la tecla ingresada:
+	static const int teclaNada = 0;
+	static const int teclaIzquierda = 1;
+	static const int teclaDerecha = 2;
+	static const int teclaArriba = 3;
+
+	// se crea por defecto con "teclaNada"
 	Evento();
+	Evento(int tecla);
 
 	// geter y seter para la tecla
-	void setTecla(string tecla);
-	string getTecla();
+	void setTecla(int tecla);
+	int getTecla();
 
 	virtual ~Evento();
 };
