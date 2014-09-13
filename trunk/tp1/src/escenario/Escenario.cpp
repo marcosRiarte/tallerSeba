7224bfd816cab172b492511a29a5f738dfc681f0
Franlopd@gmail.com <Franlopd@gmail.com@2f48152e-bfd8-70ec-e1fd-0a46247cd28f>
2014-09-13 20:55:44 +0000

    Arregle las declaraciones de escenario

--- trunk/tp1/src/escenario/Escenario.cpp
+++ trunk/tp1/src/escenario/Escenario.cpp
@@ -1,29 +1,14 @@
 /* Maneja la relacion entre Box2D y el resto del Juego.
  */
 
-#ifndef ESCENARIO_H_
-#define ESCENARIO_H_
-
 #include <vector>
-#include "../entrada/Evento.h"
-#include "../personajes/Personaje.h"
-#include "../objetos/ObjetoMapa.h"
+#include "Escenario.h"
 
-using namespace std;
+Escenario::Escenario(std::vector<Personaje> e_personajes,std::vector<ObjetoMapa> e_objetos) {
+	this->objetos = e_objetos;
+	this->personajes = e_personajes;
+}
 
-class Escenario {
-private:
-	vector<Personaje> personajes;
-	vector<ObjetoMapa> objetos;
-public:
-	//void Escenario(vector<Personaje> personajes,vector<ObjetoMapa> objetos);
+void cambiar(Evento evento) {
 
-	// Genera un nuevo de step, pone a los personajes en sus nuevas posiciones,
-	// avisando si cambiar en relacion a su posicion anterior.
-	void cambiar(Evento evento);
-
-	virtual ~Escenario();
-};
-
-
-#endif /* ESCENARIO_H_ */
+}
