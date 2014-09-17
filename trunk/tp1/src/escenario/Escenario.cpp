5780c147f55bbb91e63be41afb87ec3aa820164e
Franlopd@gmail.com <Franlopd@gmail.com@2f48152e-bfd8-70ec-e1fd-0a46247cd28f>
2014-09-17 20:42:20 +0000

    Ya se crea el escenario y los objetos, falta q guarden las referencias a si mismos en el mundo y hacer los pasos

--- trunk/tp1/src/escenario/Escenario.cpp
+++ trunk/tp1/src/escenario/Escenario.cpp
@@ -3,14 +3,113 @@
 
 #include <vector>
 #include "Escenario.h"
+#include "../objetos/Pos.h"
+#include <stdio.h>
 
-Escenario::Escenario(std::vector<Personaje>* e_personajes, std::vector<ObjetoMapa>* e_objetos) {
-	this->objetos = e_objetos;
-	this->personajes = e_personajes;
+/*
+ *	Pasa los puntos de un vector de posiciones a uno
+ *	de vértices que es la clase que maneja Box2D
+ */
+b2Vec2* PasarAVertices(std::vector<Pos>* puntos) {
+	b2Vec2 vectorDePuntos[puntos->size()];
+	for(int i=0; i<puntos->size(); i++) {
+		vectorDePuntos[i].Set(puntos->at(i).getX(), puntos->at(i).getY());
+	}
+	// Seba dijo q los array dsp hay q eliminarlos, no encontre como D:
+	return vectorDePuntos;
 }
 
-void cambiar(Evento evento) {
+/*
+ *	Agarra el punto que devuelve el contorno de un circulo
+ *	y la posicion en la que esta y devuelve su radio
+ */
+float CalcularRadio(ObjetoMapa* circulo) {
+	float distX = circulo->getPos()->getX()-circulo->getContorno()->at(0).getX();
+	float distY = circulo->getPos()->getY()-circulo->getContorno()->at(0).getY();
+	float radio = sqrt(distX*distX+distY*distY);
+	return radio;
+}
 
+Escenario::Escenario(std::vector<Personaje>* e_personajes, std::vector<ObjetoMapa>* e_objetos) {
+	objetos = e_objetos;
+	personajes = e_personajes;
+
+	// Se crea el mundo con la gravedad asignada en el .h
+	b2Vec2 gravity(gravedadX, gravedadY);
+	b2World world(gravity);
+
+	// Se crea el suelo en la posición y con el ancho y el alto que indica el .h
+	b2BodyDef groundBodyDef;
+	groundBodyDef.position.Set(posSueloX, posSueloY);
+	b2Body* groundBody = world.CreateBody(&groundBodyDef);
+	b2PolygonShape groundBox;
+	groundBox.SetAsBox(medioAnchoSuelo, medioAltoSuelo);
+	// El segundo parámetro es la densidad
+	groundBody->CreateFixture(&groundBox, 0.0f);
+
+	// Crea los personajes (se supone que todos son dinámicos)
+	for(int i=0; i<personajes->size(); i++) {
+		b2BodyDef bodyDef;
+		bodyDef.type = b2_dynamicBody;
+
+		// Setea posición y angulo
+		bodyDef.position.Set(personajes->at(i).getPosicion()->getX(), personajes->at(i).getPosicion()->getY());
+		bodyDef.angle = personajes->at(i).getRotacion();
+		b2Body* body = world.CreateBody(&bodyDef);
+
+		// El personaje es un rectangulo
+		b2FixtureDef fixtureDef;
+		b2PolygonShape forma;
+		forma.SetAsBox(medioAnchoPlayer, medioAltoPlayer);
+		fixtureDef.shape = &forma;
+
+		// La da la forma y la masa, determinando la densidad
+		body->CreateFixture(&fixtureDef);
+		b2MassData* datosDeMasa;
+		datosDeMasa->mass = masaDelPersonaje;
+		body->SetMassData(datosDeMasa);
+	}
+
+	// Crea los objetos (se supone que no todos son estáticos)
+	for(int i=0; i<objetos->size(); i++) {
+		b2BodyDef bodyDef;
+
+		// Determina si es o no estático
+		if (objetos->at(i).esEstatico()) {
+			bodyDef.type = b2_dynamicBody;
+		}
+
+		// Setea posición y angulo
+		bodyDef.position.Set(objetos->at(i).getPos()->getX(), objetos->at(i).getPos()->getY());
+		bodyDef.angle = objetos->at(i).getRotacion();
+		b2Body* body = world.CreateBody(&bodyDef);
+
+		// Determina la forma
+		b2FixtureDef fixtureDef;
+		if (true) {
+			b2PolygonShape poligono;
+			b2Vec2* vertices = PasarAVertices(objetos->at(i).getContorno());
+			int cantidadDePuntos = objetos->at(i).getContorno()->size();
+			poligono.Set(vertices, cantidadDePuntos);
+			fixtureDef.shape = &poligono;
+		} else {
+			float radio = CalcularRadio(&objetos->at(i));
+			b2CircleShape circulo;
+			circulo.m_radius = radio;
+			fixtureDef.shape = &circulo;
+		}
+
+		// La da la forma y la masa, determinando la densidad
+		body->CreateFixture(&fixtureDef);
+		b2MassData* datosDeMasa;
+		datosDeMasa->mass = objetos->at(i).getMasa();
+		body->SetMassData(datosDeMasa);
+	}
+
+	carla = &world;
+}
+
+void Escenario::cambiar(Evento evento) {
 }
 
 Escenario::~Escenario(){
--- trunk/tp1/src/escenario/Escenario.h
+++ trunk/tp1/src/escenario/Escenario.h
@@ -7,11 +7,23 @@
 #include "../personajes/Personaje.h"
 #include "../objetos/ObjetoMapa.h"
 #include "../entrada/Evento.h"
+#include <Box2D/Box2D.h>
+
+static const float gravedadX = 0;
+static const float gravedadY = -10;
+static const float posSueloX = 0;
+static const float posSueloY = -10;
+static const float medioAnchoSuelo = 50;
+static const float medioAltoSuelo = 10;
+static const float medioAnchoPlayer = 5;
+static const float medioAltoPlayer = 10;
+static const float masaDelPersonaje = 10;
 
 class Escenario {
 private:
 	std::vector<Personaje>* personajes;
 	std::vector<ObjetoMapa>* objetos;
+	b2World* carla;
 
 public:
 	Escenario(std::vector<Personaje>* e_personajes, std::vector<ObjetoMapa>* e_objetos);
