70a0d5d9da984a2b68a05cc9f89d88cb179729b5
sasavulsky@yahoo.com.ar <sasavulsky@yahoo.com.ar@2f48152e-bfd8-70ec-e1fd-0a46247cd28f>
2014-09-19 15:04:16 +0000

    Se modificaron las validaciones para hacerlas de una forma mas lógica.

--- trunk/tp1/src/parseo/ValidadorObjetos.h
+++ trunk/tp1/src/parseo/ValidadorObjetos.h
@@ -4,10 +4,9 @@
 
 class ValidadorObjetos {
 public:
-	static bool ValidarBasicos(std::string tipo, int x, int y, std::string color, int rotacion, int masa, bool estatico);
-	static bool ValidarPersonaje(int x, int y);
+	static bool ValidarBasicos(std::string tipo, int x, int y, std::string color, int rotacion, int masa, bool estatico,int ancho_un,int alto_un);
+	static bool ValidarPersonaje(int x, int y, int ancho_un, int alto_un);
 	static bool ValidarPoligono(int lados, int escala);
-	static bool ValidarRectangulo(int x, int y);
 	static bool ValidarCirculo(int diametro);
 	static bool ValidarParalelogramo(int alfa);
 	static bool ValidarTrapecio(int alfa, int beta);
