741babb9a1a3b4b331a5314830d6e65c4ef1b8c7
sasavulsky@yahoo.com.ar <sasavulsky@yahoo.com.ar@2f48152e-bfd8-70ec-e1fd-0a46247cd28f>
2014-09-19 16:05:03 +0000

    Ahora valida correctamente el color.

--- trunk/tp1/src/parseo/ValidadorObjetos.cpp
+++ trunk/tp1/src/parseo/ValidadorObjetos.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <regex>
 
 bool ValidadorObjetos::ValidarPersonaje(int x, int y,int ancho_un,int alto_un) {
         if ((x >= 0) && (x < ancho_un+1) && (y > -alto_un-1) && (y <= 0)) {
@@ -12,23 +13,33 @@ bool ValidadorObjetos::ValidarPersonaje(int x, int y,int ancho_un,int alto_un) {
         }
 }
 
+bool es_color(std::string const& s)
+{
+  return s.compare(0, 1, "#") == 0
+      && s.size() > 1
+      && s.find_first_not_of("0123456789abcdefABCDEF", 1) == std::string::npos;
+}
+
 bool ValidadorObjetos::ValidarBasicos(std::string tipo, int x, int y, std::string color, int rotacion, int masa, bool estatico,int ancho_un,int alto_un) {
+
 
         std::string str1(tipo);
         if ((((str1.compare("rectangulo")) == 0)
                         || ((str1.compare("poligono")) == 0)
                         || ((str1.compare("paralelogramo")) == 0)
                         || ((str1.compare("circulo")) == 0)
-                        || ((str1.compare("trapecio")) == 0
-                                        || ((str1.compare("circulo")) == 0))
-                        || ((str1.compare("personaje")) == 0)) && (x >= 0) && (x < ancho_un+1)
-                        && (y > -alto_un-1) && (y <= 0) && (rotacion >= 0)
+                        || ((str1.compare("trapecio")) == 0)
+                        || ((str1.compare("circulo")) == 0))
+        				&& (es_color(color))
+        				&& (x >= 0) && (x < ancho_un+1) && (y > -alto_un-1)
+        				&& (y <= 0) && (rotacion >= 0)
                         && (masa > 0 || (masa == 0 && estatico == true))) {
                 return true;
         } else {
                 return false;
         }
 }
+
 
 bool ValidadorObjetos::ValidarPoligono(int lados, int escala) {
 
