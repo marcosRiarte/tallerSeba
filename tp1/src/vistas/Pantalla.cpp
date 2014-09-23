dd9c8e5a9d8e8c73c0135082b5c61038ee721886
sasavulsky@yahoo.com.ar <sasavulsky@yahoo.com.ar@2f48152e-bfd8-70ec-e1fd-0a46247cd28f>
2014-09-23 15:25:50 +0000

    Se suben los archivos de prueba. Se modificó Pantalla. Ahora varias cosas andan como se espera.

--- tp1/src/vistas/Pantalla.cpp
+++ tp1/src/vistas/Pantalla.cpp
@@ -4,6 +4,7 @@
 #include "Pantalla.h"
 #include <SDL2/SDL2_gfxPrimitives.h>
 #include <SDL2/SDL2_rotozoom.h>
+#include <SDL_render.h>
 
 Pantalla::Pantalla(int altoPx, int anchoPx, int alto, int ancho, const char* dirImg) {
 	this->altoPx = altoPx;
@@ -12,8 +13,8 @@ Pantalla::Pantalla(int altoPx, int anchoPx, int alto, int ancho, const char* dir
 	this->ancho = ancho;
 	this->dirImg = dirImg;
 	this->vistas = new std::vector<Vista*>;
-	this->ventana = nullptr;
-	this->renderer = nullptr;
+	this->ventana = NULL;
+	this->renderer = NULL;
 	this->fondo = nullptr;
 }
 /**
@@ -25,26 +26,36 @@ void Pantalla::inicializar() throw (SDL_Excepcion){
 		const char* msg = ((std::string)"Error iniciando SDL: ").append(SDL_GetError()).c_str();
 		throw new SDL_Excepcion(msg);
 	}
-	ventana = SDL_CreateWindow("SnowBross", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, anchoPx, altoPx, SDL_WINDOW_SHOWN);
+
+	ventana = SDL_CreateWindow("SnowBross", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, anchoPx, altoPx, SDL_WINDOW_RESIZABLE);
 	if (ventana == nullptr){
 		loguer->loguear("No se pudo crear la ventana principal", Log::LOG_ERR);
 		const char* msg = ((std::string)"Error creando ventana principal: ").append(SDL_GetError()).c_str();
 		throw new SDL_Excepcion(msg);
 	}
+
 	renderer =  SDL_CreateRenderer(ventana, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_TARGETTEXTURE);
 	if (renderer == nullptr){
 		loguer->loguear("No se pudo crear el renderer", Log::LOG_ERR);
 		const char* msg = ((std::string)"Error creando el renderer: ").append(SDL_GetError()).c_str();
 		throw new SDL_Excepcion(msg);
 	}
-	this->fondo = IMG_LoadTexture(renderer, dirImg);
-	if (fondo == nullptr) {
+	SDL_Surface *direcPNG = NULL;
+	direcPNG = IMG_Load(dirImg);
+	this->fondo = SDL_CreateTextureFromSurface(renderer, direcPNG);
+	SDL_FreeSurface(direcPNG);
+
+/*	if (fondo == nullptr) {
 		loguer->loguear("No se encontró imagen de fondo", Log::LOG_ERR);
 		const char* msg =
 				((std::string) "Error cargando el fondo de pantalla: ").append(
 						SDL_GetError()).c_str();
 		throw new SDL_Excepcion(msg);
+
 	}
+*/
+
+
 }
 
 SDL_Renderer* Pantalla::getRenderer(){
