d7e8cf53cdd9eefba9aca0ea46e21fde2a87a7b2
p.gotuzzo@gmail.com <p.gotuzzo@gmail.com@2f48152e-bfd8-70ec-e1fd-0a46247cd28f>
2014-09-27 13:12:11 +0000

    Se agregan espacios y tabulaciones, nada importante.

--- trunk/tp1/src/parseo/CreadorObjetos.cpp
+++ trunk/tp1/src/parseo/CreadorObjetos.cpp
@@ -3,41 +3,41 @@
 #include "../objetos/Pos.h"
 #include "../personajes/Personaje.h"
 
-Personaje * CreadorObjetos::CrearPersonaje(int personaje_x, int personaje_y){
-	Pos *posicion = new Pos(personaje_x,personaje_y);
+Personaje * CreadorObjetos::CrearPersonaje(int personaje_x, int personaje_y) {
+	Pos *posicion = new Pos(personaje_x, personaje_y);
 	Personaje *unPersonaje = new Personaje(posicion);
 	return unPersonaje;
 }
 
-Rectangulo * CreadorObjetos::CrearRectangulo(int x, int y, int ancho, int alto,int rotacion,std::string color,int masa,bool estatico) {
-    Pos *posicion = new Pos(x,y);
-    Rectangulo * unRectangulo = new Rectangulo(estatico, color, posicion, rotacion, masa, alto, ancho);
-    return unRectangulo;
+Rectangulo * CreadorObjetos::CrearRectangulo(int x, int y, int ancho, int alto,	int rotacion, std::string color, int masa, bool estatico) {
+	Pos *posicion = new Pos(x, y);
+	Rectangulo * unRectangulo = new Rectangulo(estatico, color, posicion, rotacion, masa, alto, ancho);
+	return unRectangulo;
 }
 
-Poligono * CreadorObjetos::CrearPoligono(int x, int y, int lados, int escala, int rotacion,std::string color,int masa,bool estatico) {
-	Pos *posicion = new Pos(x,y);
+Poligono * CreadorObjetos::CrearPoligono(int x, int y, int lados, int escala, int rotacion, std::string color, int masa, bool estatico) {
+	Pos *posicion = new Pos(x, y);
 	Poligono *unPoligono = new Poligono(posicion, lados, escala, rotacion, color, masa, estatico);
 	return unPoligono;
 
 }
 
-Circulo * CreadorObjetos::CrearCirculo(int x, int y, int diametro,int rotacion,std::string color,int masa,bool estatico) {
-	Pos *posicion = new Pos(x,y);
-	Circulo* unCirculo = new Circulo(posicion, diametro, rotacion, color, masa, estatico);
+Circulo * CreadorObjetos::CrearCirculo(int x, int y, int diametro, int rotacion, std::string color, int masa, bool estatico) {
+	Pos *posicion = new Pos(x, y);
+	Circulo* unCirculo = new Circulo(posicion, diametro, rotacion, color, masa,	estatico);
 	return unCirculo;
 
 }
 
-Paralelogramo * CreadorObjetos::CrearParalelogramo(int x, int y, int base, int ladoaltura, int alfa,int rotacion,std::string color,int masa,bool estatico) {
-	Pos *posicion = new Pos(x,y);
-	Paralelogramo* unParalelogramo = new Paralelogramo(posicion,base,ladoaltura,alfa,rotacion,color,masa, estatico);
+Paralelogramo * CreadorObjetos::CrearParalelogramo(int x, int y, int base, int ladoaltura, int alfa, int rotacion, std::string color, int masa, bool estatico) {
+	Pos *posicion = new Pos(x, y);
+	Paralelogramo* unParalelogramo = new Paralelogramo(posicion, base, ladoaltura, alfa, rotacion, color, masa, estatico);
 	return unParalelogramo;
 }
 
-Trapecio * CreadorObjetos::CrearTrapecio(int x, int y, int base, int altura, int alfa, int beta,int rotacion,std::string color,int masa,bool estatico) {
-	Pos *posicion = new Pos(x,y);
-	Trapecio* unTrapecio = new Trapecio(posicion,base,altura,alfa,beta,rotacion,color,masa, estatico);
+Trapecio * CreadorObjetos::CrearTrapecio(int x, int y, int base, int altura, int alfa, int beta, int rotacion, std::string color, int masa,	bool estatico) {
+	Pos *posicion = new Pos(x, y);
+	Trapecio* unTrapecio = new Trapecio(posicion, base, altura, alfa, beta,	rotacion, color, masa, estatico);
 	return unTrapecio;
 
 }
