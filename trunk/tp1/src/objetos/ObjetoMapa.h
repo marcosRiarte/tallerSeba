79803875d2ed8c7009ff792b5b5af37a9c973601
p.gotuzzo@gmail.com <p.gotuzzo@gmail.com@2f48152e-bfd8-70ec-e1fd-0a46247cd28f>
2014-09-27 13:11:18 +0000

    Se separa la firma de la clase, de su implementación. Se agrega funcionalidad para el manejo de colores.

--- trunk/tp1/src/objetos/ObjetoMapa.h
+++ trunk/tp1/src/objetos/ObjetoMapa.h
@@ -2,15 +2,10 @@
 #define OBJETOMAPA_H_
 
 #include <vector>
-#include <map>
 #include <string>
 #include "Pos.h"
-#include "Color.h"
 #include <Box2D/Box2D.h>
 
-#ifndef M_PI
-#define M_PI           3.14159265358979323846
-#endif
 /**
  * Clase Abrstracta (no se pueden crear instancias de la misma) que sirve de base para
  * 	los demas objetos que hereden de ella. Es una interfaz
@@ -28,63 +23,34 @@ protected:
 	b2Body* linkAMundo;
 
 public:
-	void initialice(bool esEstatico, std::string color, Pos pos_cm, int rotacion, int masa) {
-		this->estatico = esEstatico;
-		this->color = color;
-		this->pos = new Pos(pos_cm);
-		this->rot = rotacion;
-		this->masa = masa;
-	}
-	bool esEstatico() {
-		return this->estatico;
-	}
+	void initialice(bool esEstatico, std::string color, Pos pos_cm, int rotacion, int masa);
 
-	bool esCirculo() {
-		return this->circulo;
-	}
+	bool esEstatico();
 
+	bool esCirculo();
 
-	float getDensidad() {
-		return this->masa/this->getArea();
-	}
+	float getDensidad();
 
-	//implementar
-	virtual	float getArea() = 0;
+	long getColor();
+	void setColor(std::string c);
 
-	std::string getColor(){
-		return color;
-	}
-	void setColor(std::string c){
-		this->color = c;
-	}
-	Pos* getPos(){
-		return pos;
-	}
-	void setPos(Pos* posicion){
-		pos = posicion;
-	}
-	int getRotacion(){
-		return rot;
-	}
-	void setRotacion(float rotacion){
-		rot = rotacion;
-	}
+	Pos* getPos();
+	void setPos(Pos* posicion);
 
-	int getMasa(){
-		return masa;
-	}
+	int getRotacion();
+	void setRotacion(float rotacion);
+
+	int getMasa();
 
 	// getter y setter de link a mundo
-	void setLinkAMundo(b2Body* link){
-		linkAMundo = link;
-	}
+	void setLinkAMundo(b2Body* link);
+	b2Body* getLinkAMundo();
 
-	b2Body* getLinkAMundo(){
-		return linkAMundo;
-	}
+	//implementar
+	virtual std::vector<Pos*>* getContorno() = 0;
+	virtual	float getArea() = 0;
 
-	virtual std::vector<Pos*>* getContorno() = 0; //este método debe redefinirse en las clases que heredan. Devuelve los vertices en caso de no ser un circulo
-	virtual ~ObjetoMapa() {};
+	virtual ~ObjetoMapa(){};
 };
 
 #endif /* OBJETOMAPA_H_ */
