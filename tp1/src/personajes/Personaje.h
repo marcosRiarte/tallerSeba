8e9ba6132618c5f094af3a31954330235c27474c
p.gotuzzo@gmail.com <p.gotuzzo@gmail.com@2f48152e-bfd8-70ec-e1fd-0a46247cd28f>
2014-09-29 22:25:14 +0000

    Se agrega imagen con sprites encuadrados. Se agregan métodos para el manejo de estados del personaje principal.

--- tp1/src/personajes/Personaje.h
+++ tp1/src/personajes/Personaje.h
@@ -6,31 +6,50 @@
 #include "../objetos/Rectangulo.h"
 
 class Personaje {
+public:
+	enum E_PERFIL {DERECHA, IZQUIERDA};
+	enum E_MOVIMIENTO {QUIETO, MOVIMIENTO, SALTANDO_SUBIENDO, SALTANDO_BAJANDO};
+	enum E_ACCION {NINGUNA, EMPUJANDO};
+
 private:
-	Pos* pos;
-	b2Body* linkAMundo;
-	float rot;
 	int ancho;
 	int alto;
-	Rectangulo *rectanguloPersonaje;
+	Pos* pos;
+	float rot;
+	b2Body* linkAMundo;
+	E_PERFIL estadoPerfil;
+	E_MOVIMIENTO estadoMovimiento;
+	E_ACCION estadoAccion;
+
+	Rectangulo *rectanguloPersonaje; //Solo para pruebas
+
 public:
 	Personaje(Pos* posicion);
 
 	// getters de dimensiones
 	int getAlto();
 	int getAncho();
+
 	// getter y setter de la posicion
 	void setPosicion(Pos* posicion);
 	Pos* getPosicion();
+
+	// devuelve verdadero si esta en esa posicion
+	bool estaEnPos(Pos* posicion);
+
 	std::vector<Pos*>* getContorno();
 	Rectangulo* getRectangulo();
+
+	// getter y setter de estado
+	void setEstado(E_PERFIL p, E_MOVIMIENTO m, E_ACCION a);
+	void setEstado(E_PERFIL p, E_MOVIMIENTO m);
+	E_PERFIL getPerfil();
+	E_MOVIMIENTO getMovimiento();
+	E_ACCION getAccion();
 
 	// getter y setter de link a mundo
 	void setLinkAMundo(b2Body* link);
 	b2Body* getLinkAMundo();
-
-	// devuelve verdadero si esta en esa posicion
-	bool estaEnPos(Pos* posicion);
 
 	virtual ~Personaje();
 };
