7b36710e70a0d6ad9f853b3f639c4a03929a4e6f
p.gotuzzo@gmail.com <p.gotuzzo@gmail.com@2f48152e-bfd8-70ec-e1fd-0a46247cd28f>
2014-09-29 22:25:14 +0000

    Se agrega imagen con sprites encuadrados. Se agregan métodos para el manejo de estados del personaje principal.

--- trunk/tp1/src/personajes/Personaje.cpp
+++ trunk/tp1/src/personajes/Personaje.cpp
@@ -49,3 +49,56 @@ void Personaje::setLinkAMundo(b2Body* link){
 b2Body* Personaje::getLinkAMundo(){
 	return linkAMundo;
 }
+
+/**
+ * \brief	Se setea el estado del personaje, mediante 3 taxonomias (incompletas para el juego final).
+ * 			E_PERFIL: 			determina el perfil a conciderar
+ * 			E_MOVIMIENTO: 		detalla que clase de movimiento está ejerciendo el personaje.
+ * 			E_ACCION:			da información sobre las interacciones que tiene el personaje con otros
+ * 								objetos del juego. Podría llamarse E_INTERACCION...es dicutible.
+ *
+ * \param	\perfil
+ * \param	\movimiento	Todos los movimientos permitidos en el juego
+ * \param	\accion		Se utiliza para las interacciones del juego, por ahora solo tomamos
+ * 						el caso de que este empujando un objeto o simplemente no haciendo nada
+ */
+void Personaje::setEstado(E_PERFIL perfil, E_MOVIMIENTO movimiento, E_ACCION accion){
+	estadoPerfil = perfil;
+	estadoMovimiento = movimiento;
+	estadoAccion = accion;
+}
+
+/**
+ * \brief	Al igual que el método setEstado(E_PERFIL, E_MOVIMIENTO, E_ACCION) se setea el estado
+ * 			del personaje. Pero se toma para el caso de E_ACCION, el valor por defecto que es "NINGUNA"
+ *
+ * \param	\perfil
+ * \param	\movimiento	Todos los movimientos permitidos en el juego
+ */
+void Personaje::setEstado(E_PERFIL perfil, E_MOVIMIENTO movimiento){
+	estadoPerfil = perfil;
+	estadoMovimiento = movimiento;
+	estadoAccion = E_ACCION::NINGUNA;
+}
+
+/**
+ * \return	Estado relacionado con el perfil del personaje
+ */
+Personaje::E_PERFIL Personaje::getPerfil(){
+	return estadoPerfil;
+}
+
+/**
+ * \return	Estado relacionado con el movimiento actual del personaje
+ */
+Personaje::E_MOVIMIENTO Personaje::getMovimiento(){
+	return estadoMovimiento;
+}
+
+/**
+ * \return	Estado relacionado con la accion que realiza el personaje
+ */
+Personaje::E_ACCION Personaje::getAccion(){
+	return estadoAccion;
+}
+
