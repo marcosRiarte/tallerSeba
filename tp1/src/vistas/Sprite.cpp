9a5193678711b032a1f0d0d3ecfecf8b162c4c70
sasavulsky@yahoo.com.ar <sasavulsky@yahoo.com.ar@2f48152e-bfd8-70ec-e1fd-0a46247cd28f>
2014-10-01 21:07:59 +0000

    se corrigen

--- tp1/src/vistas/Sprite.cpp
+++ tp1/src/vistas/Sprite.cpp
@@ -10,17 +10,20 @@ SDL_Rect* crearCuadro(int x, int y, int w, int h){
 }
 
 Sprite::Sprite(){
-	this->CaminandoDer = new std::vector<SDL_Rect*>();
+	this->CayendoDer = new std::vector<SDL_Rect*>();
 	this->SaltandoDer = new std::vector<SDL_Rect*>();
-	this->SaltandoDiagDer = new std::vector<SDL_Rect*>();
+	this->CaminandoDer = new std::vector<SDL_Rect*>();
 	this->Quieto = new std::vector<SDL_Rect*>();
 
 	this->CaminandoIzq = new std::vector<SDL_Rect*>();
 	this->SaltandoIzq = new std::vector<SDL_Rect*>();
-	this->SaltandoDiagIzq = new std::vector<SDL_Rect*>();
+	this->CayendoIzq = new std::vector<SDL_Rect*>();
 
 
 	//Quieto
+	this->Quieto->push_back(crearCuadro(165,87,48,50));
+	this->Quieto->push_back(crearCuadro(165,87,48,50));
+	this->Quieto->push_back(crearCuadro(165,87,48,50));
 	this->Quieto->push_back(crearCuadro(165,87,48,50));
 
 	//Izquierda
@@ -29,15 +32,19 @@ Sprite::Sprite(){
 	this->CaminandoIzq->push_back(crearCuadro(38,16,22,24));
 	this->CaminandoIzq->push_back(crearCuadro(62,16,20,24));
 	this->CaminandoIzq->push_back(crearCuadro(94,16,18,24));
+	this->CaminandoIzq->push_back(crearCuadro(94,16,18,24));
 
 	//salto vertical
-	this->SaltandoIzq->push_back(crearCuadro(118,10,21,30));
+	this->CayendoIzq->push_back(crearCuadro(118,10,21,30));
+	this->CayendoIzq->push_back(crearCuadro(118,10,21,30));
+	this->CayendoIzq->push_back(crearCuadro(118,10,21,30));
+	this->CayendoIzq->push_back(crearCuadro(118,10,21,30));
 
 	//salto diagonal
-	this->SaltandoDiagIzq->push_back(crearCuadro(150,16,23,25));
-	this->SaltandoDiagIzq->push_back(crearCuadro(182,19,24,21));
-	this->SaltandoDiagIzq->push_back(crearCuadro(216,16,21,24));
-	this->SaltandoDiagIzq->push_back(crearCuadro(246,16,25,24));
+	this->SaltandoIzq->push_back(crearCuadro(150,16,23,25));
+	this->SaltandoIzq->push_back(crearCuadro(182,19,24,21));
+	this->SaltandoIzq->push_back(crearCuadro(216,16,21,24));
+	this->SaltandoIzq->push_back(crearCuadro(246,16,25,24));
 
 	//Derecha
 
@@ -45,30 +52,29 @@ Sprite::Sprite(){
 	this->CaminandoDer->push_back(crearCuadro(807,16,17,24));
 	this->CaminandoDer->push_back(crearCuadro(780,16,20,24));
 	this->CaminandoDer->push_back(crearCuadro(750,16,18,24));
+	this->CaminandoDer->push_back(crearCuadro(807,16,17,24));
 
 	//salto vertical
-	this->SaltandoDer->push_back(crearCuadro(723,10,21,30));
+	this->CayendoDer->push_back(crearCuadro(723,10,21,30));
+	this->CayendoDer->push_back(crearCuadro(723,10,21,30));
+	this->CayendoDer->push_back(crearCuadro(723,10,21,30));
+	this->CayendoDer->push_back(crearCuadro(723,10,21,30));
 
 	//salto diagonal
-	this->SaltandoDiagDer->push_back(crearCuadro(689,16,23,25));
-	this->SaltandoDiagDer->push_back(crearCuadro(656,19,24,21));
-	this->SaltandoDiagDer->push_back(crearCuadro(626,15,22,25));
-	this->SaltandoDiagDer->push_back(crearCuadro(591,16,25,24));
+	this->SaltandoDer->push_back(crearCuadro(689,16,23,25));
+	this->SaltandoDer->push_back(crearCuadro(656,19,24,21));
+	this->SaltandoDer->push_back(crearCuadro(626,15,22,25));
+	this->SaltandoDer->push_back(crearCuadro(591,16,25,24));
 }
 
 std::vector<SDL_Rect*>* Sprite::listaDeCuadros(std::string tipoMov){
-	if(tipoMov =="Quieto"){
-		return this->Quieto;
-	}
 
-	if(tipoMov.compare("CaminandoIzq")==0){
-		return CaminandoIzq;
-	}
+	if(tipoMov=="CaminandoIzq"){return CaminandoIzq;}
 	if(tipoMov=="SaltandoIzq"){return SaltandoIzq;}
-	if(tipoMov=="SaltandoDiagIzq"){return SaltandoDiagIzq;}
+	if(tipoMov=="CayendoIzq"){return CayendoIzq;}
 	if(tipoMov=="CaminandoDer"){return CaminandoDer;}
 	if(tipoMov=="SaltandoDer"){return SaltandoDer;}
-	if(tipoMov=="SaltandoDiagDer"){return SaltandoDiagDer;}
+	if(tipoMov=="CayendoDer"){return CayendoDer;}
 	return this->Quieto;
 }
 
