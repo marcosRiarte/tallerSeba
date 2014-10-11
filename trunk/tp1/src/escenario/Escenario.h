6d7597940d00f2b548d87b76276e82730af0ebcd
Franlopd@gmail.com <Franlopd@gmail.com@2f48152e-bfd8-70ec-e1fd-0a46247cd28f>
2014-10-11 13:12:37 +0000

    organice las constantes de escenario para que esten toda juntas

--- trunk/tp1/src/escenario/Escenario.h
+++ trunk/tp1/src/escenario/Escenario.h
@@ -9,27 +9,6 @@
 #include "../parseo/Config.h"
 #include "MyContactListener.h"
 
-// Constantes del mundo en Box2D
-static const float
-GRAVEDAD_X = 0,
-GRAVEDAD_Y = -50,
-MEDIO_ALTO_SUELO = 2,
-MEDIO_ALTO_TECHO = 1,
-MEDIO_ANCHO_PARED = 2,
-DENSIDAD_DEL_PERSONAJE = 0.31,
-FRICCION_DEL_PERSONAJE = 0.4,
-FRICCION_DE_OBJETO = 0.4,
-FRICCION_CERO = 0.4,
-IMPULSO_IZQ_X = -5000,
-IMPULSO_IZQ_Y = 0,
-IMPULSO_DER_X = 5000,
-IMPULSO_DER_Y = 0,
-IMPULSO_ARR_X = 0,
-IMPULSO_ARR_Y = 650000;
-static const float32 TIME_STEP = 1/60.0;
-static const int32 VELOCITY_ITERATIONS = 8;
-static const int32 POSITION_ITERATIONS = 3;
-
 class Escenario {
 private:
 	std::vector<Personaje*>* personajes;
