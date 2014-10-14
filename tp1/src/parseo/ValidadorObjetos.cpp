b9a6ddb0101fcc347f756ff5fe0fa31b06eceace
sasavulsky@yahoo.com.ar <sasavulsky@yahoo.com.ar@2f48152e-bfd8-70ec-e1fd-0a46247cd28f>
2014-10-14 14:53:31 +0000

    Se corrigió el logueo en las Validaciones. Ahora loguea como un campeón!

--- tp1/src/parseo/ValidadorObjetos.cpp
+++ tp1/src/parseo/ValidadorObjetos.cpp
@@ -19,42 +19,57 @@ bool es_color(std::string const& s) {
 	return s.compare(0, 1, "#") == 0 && s.size() > 1 && s.find_first_not_of("0123456789abcdefABCDEF", 1) == std::string::npos;
 }
 
-void ValidadorObjetos::ValidarBasicos(std::string *tipo, int *x, int *y, std::string *color, int *rotacion, int *masa, bool estatico,int ancho_un,int alto_un){
+void ValidadorObjetos::ValidarBasicos(std::string *tipo, int *x, int *y,
+		std::string *color, int *rotacion, int *masa, bool estatico,
+		int ancho_un, int alto_un) {
 	std::string mensaje;
 	std::string str1(*tipo);
-	if (((str1.compare("rectangulo")) == 0) || ((str1.compare("poligono")) == 0)
+	if (!(((str1.compare("rectangulo")) == 0)
+			|| ((str1.compare("poligono")) == 0)
 			|| ((str1.compare("paralelogramo")) == 0)
 			|| ((str1.compare("circulo")) == 0)
 			|| ((str1.compare("trapecio")) == 0)
-			|| ((str1.compare("circulo")) == 0)) {
-		if (es_color(*color)) {
-			if ((*x >= 0) && (*x < ancho_un + 1) && (*y > -alto_un - 1) && (*y <= 0)) {
-				if (*rotacion >= 0) {
-					if (!(masa > 0 || (masa == 0 && estatico == true))) {
-						mensaje = "la masa debe ser mayor a 0 o ser 0 y el objeto estático, se toma masa 5";
-						*masa=5;
-					}
-				} else {
-					mensaje = "La rotación debe ser mayor o igual a 0, se toma rotacion=0";
-					*rotacion=0;
-				}
-			} else {
-				mensaje = "Objeto fuera de pantalla, se coloca en una posicion arbitraria";
-				*x=40;
-				*y=-50;
-			}
-		} else {
-			mensaje = "El color no representa una sintáxis válida, se crea color por defecto";
-			*color="#FF00FF";
-		}
-	}
-	else{
-		mensaje = "El tipo ", str1, " no es un tipo de objeto válido, se crea rectangulo por defecto";
+			|| ((str1.compare("circulo")) == 0))) {
+
+		mensaje = "El tipo "+*tipo+" no es un tipo de objeto válido, se crea rectangulo por defecto";
 		*tipo = "rectangulo";
+		//Paso a const char* el string del mensaje.
+			const char * c = mensaje.c_str();
+			loguer->loguear(c, Log::LOG_TIPO::LOG_WAR);
 	}
-	//Paso a const char* el string del mensaje.
-	const char * c = mensaje.c_str();
-	loguer->loguear(c, Log::LOG_TIPO::LOG_WAR);
+	if (!(es_color(*color))) {
+		mensaje =
+				"El color "+*color+" no representa una sintáxis válida, se crea color por defecto";
+		//Paso a const char* el string del mensaje.
+			const char * c = mensaje.c_str();
+			loguer->loguear(c, Log::LOG_TIPO::LOG_WAR);
+
+		*color = "#FF00FF";
+	}
+	if (!((*x >= 0) && (*x < ancho_un + 1) && (*y > -alto_un - 1) && (*y <= 0))) {
+		mensaje =
+				"Objeto fuera de pantalla, se coloca en una posicion arbitraria";
+		//Paso a const char* el string del mensaje.
+			const char * c = mensaje.c_str();
+			loguer->loguear(c, Log::LOG_TIPO::LOG_WAR);
+		*x = 40;
+		*y = -50;
+	}
+	if (!(*rotacion >= 0)) {
+		mensaje = "La rotación debe ser mayor o igual a 0, se toma rotacion=0";
+		//Paso a const char* el string del mensaje.
+			const char * c = mensaje.c_str();
+			loguer->loguear(c, Log::LOG_TIPO::LOG_WAR);
+		*rotacion = 0;
+	}
+	if (!(masa > 0 || (masa == 0 && estatico == true))) {
+		mensaje =
+				"la masa debe ser mayor a 0 o ser 0 y el objeto estático, se toma masa 5";
+		//Paso a const char* el string del mensaje.
+			const char * c = mensaje.c_str();
+			loguer->loguear(c, Log::LOG_TIPO::LOG_WAR);
+		*masa = 5;
+	}
 }
 
 void ValidadorObjetos::ValidarPoligono(int* lados, float* escala) {
