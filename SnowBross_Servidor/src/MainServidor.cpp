a82ab77f325735ca4ad90650bed40192e0fc63ce
Franlopd@gmail.com <Franlopd@gmail.com@2f48152e-bfd8-70ec-e1fd-0a46247cd28f>
2014-11-11 01:39:14 +0000

    server acomodado

--- SnowBross_Servidor/src/MainServidor.cpp
+++ SnowBross_Servidor/src/MainServidor.cpp
@@ -27,55 +27,49 @@ typedef struct EDatos {
 
 // Hilo que envia los eventos de entrada en teclado
 DWORD WINAPI enviarDatos(void * param) {
-	EDatos* datos;
-	datos = (EDatos*) param;
+	PEDATOS datos;
+	datos = (PEDATOS) param;
 
 	PaqueteACliente paquete;
 	paquete.tipoPaquete = TipoPaquete::ACTUALIZACION;
 
 	// No se con q condicion cerrar el server, dsp le pongo corte
-	while(true) {
+	while (true) {
 		Sleep(30);
-		// Si se enviaron suficientes eventos se vacian las listas
-		if (datos->vectorDeListaDeEventos->size() > 0.8*datos->cantDeClientes) {
-			datos->escenario->cambiar(*(datos->vectorDeListaDeEventos),*(datos->vectorDeID));
-			// Esto no deberia perder memoria, pero si se pierde memoria en algun lugar ver aca
-			datos->vectorDeListaDeEventos->clear();
-			datos->vectorDeID->clear();
 
-			// Se arma el paquete para enviar
-			std::vector<Personaje*> personajes = datos->escenario->getPersonajes();
-			paquete.contadorPersonaje = personajes.size();
-			for (unsigned int j = 0; j < personajes.size(); j++) {
-				paquete.paquetePersonaje[j].estado = personajes.at(j)->getEstado();
-				paquete.paquetePersonaje[j].id = personajes.at(j)->getID();
-				Pos pos = personajes.at(j)->getPos().ySimetrico();
-				paquete.paquetePersonaje[j].x = pos.getX();
-				paquete.paquetePersonaje[j].y = pos.getY();
+		// Se arma el paquete para enviar
+		std::vector<Personaje*> personajes = datos->escenario->getPersonajes();
+		paquete.contadorPersonaje = personajes.size();
+		for (unsigned int j = 0; j < personajes.size(); j++) {
+			paquete.paquetePersonaje[j].estado = personajes.at(j)->getEstado();
+			paquete.paquetePersonaje[j].id = personajes.at(j)->getID();
+			Pos pos = personajes.at(j)->getPos().ySimetrico();
+			paquete.paquetePersonaje[j].x = pos.getX();
+			paquete.paquetePersonaje[j].y = pos.getY();
+		}
+		std::vector<ObjetoMapa*> objetos = datos->escenario->getObjetos();
+		paquete.contadorObjetos = objetos.size();
+		for (unsigned int j = 0; j < objetos.size(); j++) {
+			ObjetoMapa* o = objetos.at(j);
+			Pos p = o->getPos().ySimetrico();
+			std::vector<Pos> contorno = o->getContorno();
+			paquete.paqueteObjeto[j].id = o->getID();
+			paquete.paqueteObjeto[j].x = p.getX();
+			paquete.paqueteObjeto[j].y = p.getY();
+			paquete.paqueteObjeto[j].cantidadVertices = contorno.size();
+			for (unsigned k = 0; k < contorno.size(); k++) {
+				Pos pAux = contorno.at(k).ySimetrico();
+				paquete.paqueteObjeto[j].vx[k] = pAux.getX();
+				paquete.paqueteObjeto[j].vy[k] = pAux.getY();
 			}
-			std::vector<ObjetoMapa*> objetos = datos->escenario->getObjetos();
-			paquete.contadorObjetos = objetos.size();
-			for (unsigned int j = 0; j < objetos.size(); j++) {
-				ObjetoMapa* o = objetos.at(j);
-				Pos p = o->getPos().ySimetrico();
-				std::vector<Pos> contorno = o->getContorno();
-				paquete.paqueteObjeto[j].id = o->getID();
-				paquete.paqueteObjeto[j].x = p.getX();
-				paquete.paqueteObjeto[j].y = p.getY();
-				paquete.paqueteObjeto[j].cantidadVertices = contorno.size();
-				for(unsigned k = 0; k < contorno.size(); k++){
-					Pos pAux = contorno.at(k).ySimetrico();
-					paquete.paqueteObjeto[j].vx[k] = pAux.getX();
-					paquete.paqueteObjeto[j].vy[k] = pAux.getY();
-				}
-				paquete.paqueteObjeto[j].color = o->getColor();
-				paquete.paqueteObjeto[j].esEstatico = o->esEstatico();
-				paquete.paqueteObjeto[j].rotacion = o->getRotacion();
-			}
-			// Se envia a todos los clientes
-			for(int i=0; i<datos->cantDeClientes; i++ ) {
-				Servidor::enviar(Servidor::sock[i],paquete);
-			}
+			paquete.paqueteObjeto[j].color = o->getColor();
+			paquete.paqueteObjeto[j].esEstatico = o->esEstatico();
+			paquete.paqueteObjeto[j].rotacion = o->getRotacion();
+		}
+
+		// Se envia a todos los clientes
+		for (int i = 0; i < datos->cantDeClientes; i++) {
+			Servidor::enviar(Servidor::sock[i], paquete);
 		}
 	}
 
@@ -84,15 +78,8 @@ DWORD WINAPI enviarDatos(void * param) {
 
 // Hilo que recibe los datos de los cambios en el escenario y los muestra
 DWORD WINAPI recibirDatos(void * param) {
-	RDatos* datos;
-	datos = (RDatos*) param;
-
-	std::vector<std::vector<Evento>*>* vectorDeListaDeEventosA = datos->vectorDeListaDeEventos;
-	std::vector<int>* vectorDeIDA = datos->vectorDeID;
-	int numeroDeSockA = datos->numeroDeSock;
-	int personajeIDA = datos->personajeID;
-
-
+	PRDATOS datos;
+	datos = (PRDATOS) param;
 
 	PaqueteAServidor paquete;
 	// Se recorre los datos recibidos cambiando rot y pos y dsp se muestra.
@@ -154,35 +141,34 @@ int main(int argc, char** argv) {
 	std::vector<int>* vDeID = new std::vector<int>();
 	std::vector<std::vector<Evento>*>* vDeListaDeEventos = new std::vector<std::vector<Evento>*>();
 	HANDLE  vectorDeHilos[cantidadDeClientes];
+	PRDATOS rdatos[cantidadDeClientes];
 
-	RDatos* rdatos = new RDatos();
-	rdatos->vectorDeID = vDeID;
-	rdatos->vectorDeListaDeEventos = vDeListaDeEventos;
-	rdatos->numeroDeSock = 0;
-	rdatos->personajeID = 0;
-	vectorDeHilos[0] = CreateThread(0, 0, recibirDatos, rdatos, 0, 0);
-
-
-	/*
 	for(unsigned int i=0; i<cantidadDeClientes; i++ ) {
-		PRDATOS rdatos = new RDatos();
-		rdatos->vectorDeID = vDeID;
-		rdatos->vectorDeListaDeEventos = vDeListaDeEventos;
+		rdatos[i] = new RDatos();
+		rdatos[i]->vectorDeID = vDeID;
+		rdatos[i]->vectorDeListaDeEventos = vDeListaDeEventos;
 		// Esto esta hecho asi, pq no se de q manera puedo poner el mismo numero de sock al id de personaje
 		// TODO
-		rdatos->numeroDeSock = i;
-		rdatos->personajeID = i;
-		vectorDeHilos[i] = CreateThread(0, 0, recibirDatos, rdatos, 0, 0);
-	}*/
+		rdatos[i]->numeroDeSock = i;
+		rdatos[i]->personajeID = i;
+		vectorDeHilos[i] = CreateThread(0, 0, recibirDatos, rdatos[i], 0, 0);
+	}
 
-	EDatos* datos = new EDatos();
+	PEDATOS datos = new EDatos();
 	datos->vectorDeID = vDeID;
 	datos->vectorDeListaDeEventos = vDeListaDeEventos;
 	datos->cantDeClientes = cantidadDeClientes;
 	datos->escenario = esc;
 	HANDLE hiloEnviaDatos = CreateThread(0, 0, enviarDatos, datos, 0, 0);
 
-
+	while (true) {
+		Sleep(30);
+		// TODO ver como se vacian las listas
+		esc->cambiar(*(datos->vectorDeListaDeEventos),	*(datos->vectorDeID));
+		// Esto no deberia perder memoria, pero si se pierde memoria en algun lugar ver aca
+		datos->vectorDeListaDeEventos->clear();
+		datos->vectorDeID->clear();
+	}
 
 	WaitForSingleObject(hiloEnviaDatos, INFINITE);
 
@@ -192,6 +178,8 @@ int main(int argc, char** argv) {
 	delete configuracion;
 	for(unsigned int i=0; i<cantidadDeClientes; i++ ) {
 		CloseHandle(vectorDeHilos[i]);
+		delete rdatos[i];
 	}
 	CloseHandle(hiloEnviaDatos);
+	delete datos;
 }
